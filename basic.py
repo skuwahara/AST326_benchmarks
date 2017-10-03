d103a36370e54e99beecfd217c6a61cc1e308e0e
andrewmaksymowsky <andyj010@gmail.com>
2017-10-02 23:32:45 -0500

    Pretesting

--- basic.py
+++ basic.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
-test_img = np.load("test_img.npy")
-
-#Supporting Functions
 def gen_neighbours(img, x, y):
 	
 	width = img.shape[0]-1
 	height = img.shape[1]-1	
 
-	#coordinates to examine
 	coordinates = []
 	for i in [x-1, x, x+1]:
 		for j in [y-1, y, y+1]:
@@ -20,8 +17,18 @@ def gen_neighbours(img, x, y):
 				coordinates.append((i,j))
 		
 	return coordinates
-				
+			
+def neighbours_elevated(neighbours, img, threshold):
+	for i in neighbours:
+		if(img[i] > threshold/2.0):	
+			return 1
+	return 0
 
+def filter_out_neighbours(img, neighbours):
+	for i in neighbours:
+		img[i] = 0.0
+	return img
+	
 #Basic Algorithm O(n^2)
 def basic(img, threshold=50):
 
@@ -30,12 +37,19 @@ def basic(img, threshold=50):
 
 	for x in range(0, width):
 		for y in range(0, height):
-			if(img[x,y] > (threshold/2.0) ):
+			if(img[x,y] > (threshold) ):
 				neighbours = gen_neighbours(img, x, y)
-				print neighbours
-				#if(neighbours_elevated(neighbours)):
-				#	filter_out_neighbours(img, x, y)
+				if(neighbours_elevated(neighbours, img, threshold)):
+					img = filter_out_neighbours(img, neighbours)
 			
+	return img
 
+# Testing
+def unit_test():
+	test_img = np.load("test_img.npy")
+	filtered_image = basic(test_img)
 
-basic(test_img)
+	
+	return
+
+unit_test()
